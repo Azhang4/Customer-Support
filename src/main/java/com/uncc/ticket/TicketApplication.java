b4e92c398d54e479ca460c57a231b4697bf3b616
Azhang4 <azhang4@uncc.edu>
2020-10-20 16:12:56 -0400

    first commit

--- src/main/java/com/uncc/ticket/TicketApplication.java
+++ src/main/java/com/uncc/ticket/TicketApplication.java
@@ -3,6 +3,7 @@ package com.uncc.ticket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class TicketApplication {
